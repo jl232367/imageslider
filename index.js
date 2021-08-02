2d051aa5c94fc74a876be8d25538c6424ed631d1
jsl.luallen@gmail.com <jsl.luallen@gmail.com>
2021-08-02 10:00:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Want to make and image slider that can take in an array of images and Advance through them, go backwards throw them, allow for jumping to specific pictures, and advance automatically every 5 seconds. 
+
+
+// Take and array of image url (file reference) to iterate through and generate and also the parent element id for the purpose of storage
+const imgSlider = (imgArray, imageSliderParentElementId) => {
+
+}
